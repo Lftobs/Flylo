ac4f834f1172ed0834e882ddc21e60349271ec0c
tobsdev12 <tobbytobs1@gmail.com>
2023-11-09 04:31:25 +0100

    first push

---
+++ tailwind.config.mjs
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		extend: {
+			colors : {
+				'bg' :{
+					intro: 'hsl(217, 28%, 15%)', //intro
+					main:'hsl(218, 28%, 13%)', //main
+					footer: ' hsl(216, 53%, 9%)',
+					tma: ' hsl(219, 30%, 18%)'
+				},
+			}
+		},
+	},
+	plugins: [],
+}
