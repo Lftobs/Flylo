c80e37aa38be6a4b00e876777b6ef5747a768f76
tobsdev12 <tobbytobs1@gmail.com>
2023-11-15 11:49:22 +0100

    updates

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -7,13 +7,18 @@ export default {
 				'bg' :{
 					intro: 'hsl(217, 28%, 15%)', //intro
 					main:'hsl(218, 28%, 13%)', //main
-					footer: ' hsl(216, 53%, 9%)',
-					tma: ' hsl(219, 30%, 18%)'
+					footer: 'hsl(216, 53%, 9%)',
+					tma: 'hsl(219, 30%, 18%)'
 				},
+				'blue' : {
+					100: ' hsl(176, 68%, 64%)',
+					200: 'hsl(198, 60%, 50%)',
+					err: 'hsl(0, 100%, 63%)'
+				}
 			},
 			fontFamily : {
-				   raleway : [],
-				   openSans : []
+				   raleway : ['Raleway', 'sans-serif'],
+				   openSans : ['Open Sans', 'sans-serif']
 			},
 		},
 	},
